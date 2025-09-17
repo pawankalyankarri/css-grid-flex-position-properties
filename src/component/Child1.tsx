6f8f8333b6476130c41aa4d2d40799c9d8cdc1bb
pawankalyan <pawankalyankarri22@gmail.com>
2025-09-17 13:17:01 +0530

    created two layouts using grid

---
+++ src/component/Child1.tsx
@@ -0,0 +1,14 @@
+import './styles/child1.css'
+const Child1 = () => {
+    return(
+        <div className="child1">
+            <div className="header" >Header</div>
+            <div className="sidebar">sidebar</div>
+            <div className="main">main</div>
+            <div className="aside">aside</div>
+            <div className="footer">footer</div>
+        </div>
+    )
+}
+
+export default Child1;
---
+++ src/component/Child2.tsx
@@ -0,0 +1,16 @@
+import './styles/child2.css'
+const Child2 = () => {
+    return(
+        <div className="child2">
+            <div className="div1">div1</div>
+            <div className="div2">div2</div>
+            <div className="div3">div3</div>
+            <div className="div4">div4</div>
+            <div className="div5">div5</div>
+            <div className="div6">div6</div>
+            <div className="div7">div7</div>
+            <div className="div8">div8</div>
+        </div> 
+    )
+}
+export default Child2;
