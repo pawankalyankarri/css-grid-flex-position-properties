baf4f155bb29628fa4b40feeacbeba8a1c3834e0
pawankalyan <pawankalyankarri22@gmail.com>
2025-09-17 16:22:12 +0530

    added one more layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import Child1 from './component/Child1'
 import Navbar from './component/Navbar'
 import Child2 from './component/child2'
+import Child3 from './component/Child3'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='c1' element={<Child1/>}/>
       <Route path='c2' element={<Child2/>}/>
+      <Route path='c3' element={<Child3/>}/>
     </Routes>
   </div>
   )
--- src/component/Child3.tsx
+++ src/component/Child3.tsx
@@ -1,19 +1,20 @@
+import "./styles/child3.css";
 const Child3 = () => {
-    return(
-        <div className="child3">
-            <div>
-                <div className="box1"></div>
-                <div className="box2"></div>
-                <div className="box3"></div>
-                <div className="box4"></div>
-            </div>
-            <div>
-                <div className="gbox1"></div>
-                <div className="gbox2"></div>
-                <div className="gbox3"></div>
-                <div className="gbox4"></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="child3">
+      <div className="box1"></div>
+      <div className="box2"></div>
+      <div className="box3">
+        <div className="div1"></div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+        <div className="div4"></div>
+      </div>
+      <div className="box4"></div>
+      <div className="box5"></div>
+      <div className="box6"></div>
+      <div className="box7"></div>
+    </div>
+  );
+};
 export default Child3;
